53c75d7d6f3b6feb374be52006c8222b7a007c39
Gianluca Napoletano <gianluca@192.168.1.51>
2024-07-16 12:11:30 +0200

    handle navigation

--- app/src/main/java/com/example/themoviedbclient/presentation/view/baseclass/ActivityBaseClass.kt
+++ app/src/main/java/com/example/themoviedbclient/presentation/view/baseclass/ActivityBaseClass.kt
@@ -2,7 +2,15 @@ package com.example.themoviedbclient.presentation.view.baseclass
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.themoviedbclient.R
 import com.example.themoviedbclient.databinding.BaseActivityLayoutBinding
 
 abstract class ActivityBaseClass: AppCompatActivity() {
@@ -13,10 +21,17 @@ abstract class ActivityBaseClass: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         baseActivityLayoutBinding = BaseActivityLayoutBinding.inflate(layoutInflater)
         setContentView(baseActivityLayoutBinding.root)
+
+        val navView = baseActivityLayoutBinding.navView
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navView.menu)
+
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        baseActivityLayoutBinding.navView.setupWithNavController(navController)
+
         afterOnCreate()
     }
 
     open fun afterOnCreate() {}
-
-
 }
