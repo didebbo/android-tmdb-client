0601925f0a75bd8e08f254b4f65eb17bb0ca3607
Gianluca Napoletano <gianluca@192.168.1.51>
2024-07-17 09:33:08 +0200

    refactoring trendingMovieAdapter

--- app/src/main/java/com/example/themoviedbclient/presentation/view/fragments/trendingMovies/TrendingMoviesAdapter.kt
+++ app/src/main/java/com/example/themoviedbclient/presentation/view/fragments/trendingMovies/TrendingMoviesAdapter.kt
@@ -3,34 +3,36 @@ package com.example.themoviedbclient.presentation.view.fragments.trendingMovies
 import android.view.View
 import com.bumptech.glide.Glide
 import com.example.themoviedbclient.R
-import com.example.themoviedbclient.databinding.TrendingMovieRecyclerItemViewBinding
+import com.example.themoviedbclient.databinding.SmallItemRecyclerViewBinding
 import com.example.themoviedbclient.presentation.view.baseclass.util.BaseRecyclerViewAdapter
 import com.example.themoviedbclient.presentation.view.baseclass.util.BaseViewHolder
 import com.example.themoviedbclient.presentation.view.baseclass.util.BaseViewHolderItem
 
-class TrendingMoviesAdapter(data: List<TrendingMoviesItem>): BaseRecyclerViewAdapter<TrendingMoviesAdapter.TrendingMoviesViewHolder,TrendingMoviesAdapter.TrendingMoviesItem>(data){
+class TrendingMoviesItem(
+    val fullPosterPath: String,
+    val title: String,
+    val overview: String
+): BaseViewHolderItem()
 
-    class TrendingMoviesItem(
-        val fullPosterPath: String,
-        val title: String,
-        val overview: String
-    ): BaseViewHolderItem()
+sealed class TrendingMovieViewHolder(itemView: View): BaseViewHolder<View,TrendingMoviesItem>(itemView)  {
+    class SmallItemTrendingMovieViewHolder(itemView: View): TrendingMovieViewHolder(itemView) {
 
-    inner class TrendingMoviesViewHolder(itemView: View): BaseViewHolder<View,TrendingMoviesItem>(itemView) {
-
-        private val binding: TrendingMovieRecyclerItemViewBinding = TrendingMovieRecyclerItemViewBinding.bind(itemView)
+        private val binding: SmallItemRecyclerViewBinding = SmallItemRecyclerViewBinding.bind(itemView)
         override fun bind(item: TrendingMoviesItem) {
             binding.titleTextView.text = item.title
             binding.overviewTextView.text = item.overview
             Glide.with(binding.root).load(item.fullPosterPath).into(binding.posterImage)
         }
     }
+}
 
-    override fun createViewHolder(view: View): TrendingMoviesViewHolder {
-        return TrendingMoviesViewHolder(view)
+class TrendingMoviesAdapter(data: List<TrendingMoviesItem>): BaseRecyclerViewAdapter<TrendingMovieViewHolder,TrendingMoviesItem>(data){
+
+    override fun createViewHolder(view: View): TrendingMovieViewHolder {
+        return TrendingMovieViewHolder.SmallItemTrendingMovieViewHolder(view)
     }
 
     override fun getLayoutFrom(position: Int): Int {
-        return R.layout.trending_movie_recycler_item_view
+        return R.layout.small_item_recycler_view
     }
 }
