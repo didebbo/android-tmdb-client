7b99e2321a2c6f2339bb3db90289d4456f956428
Gianluca Napoletano <gianluca@192.168.1.51>
2024-07-18 17:35:51 +0200

    add viewPager2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.viewpager2)
 
     // Retrofit2
     implementation(libs.retrofit)
